5d3e25829619594c530dddcab57e7471a70a1935
Marwa ELmasry <marwaelmasry592002@gmail.com>
2025-11-08 16:45:58 +0200

    added a simple splash screen

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -21,7 +21,7 @@ class DocApp extends StatelessWidget {
         ),
         onGenerateRoute: appRouter.generateRoute,
         debugShowCheckedModeBanner: false,
-        initialRoute: Routes.onBoardingScreen,
+        initialRoute: Routes.splashScreen,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_project/core/routing/app_routes.dart';
 import 'doc_app.dart';
- // TODO: Mo2a testing 
 void main() {
   runApp(DocApp(appRouter: AppRoutes(),));
 }
