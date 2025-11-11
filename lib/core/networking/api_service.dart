53710f4c2cea841bc4e7d37de6ab46417266458f
Marwa ELmasry <marwaelmasry592002@gmail.com>
2025-11-11 18:23:52 +0200

    added dependency inj added api/login added login cubit && states added login regex -> validation

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:new_project/core/networking/api_constants.dart';
+import 'package:new_project/features/login/data/models/login_request_body.dart';
+import 'package:new_project/features/login/data/models/login_response_body.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+
+part 'api_service.g.dart';
+
+ ///retrofit annotation
+@RestApi(baseUrl: ApiConstants.apiBaseUrl)
+abstract class ApiService {
+  // i injected it into dio -> make api depends on dio
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.login)
+  Future<LoginResponseBody> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
+}
+
+
+ 
--- lib/features/login/ui/widgets/email_and_password.dart
+++ lib/features/login/ui/widgets/email_and_password.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:new_project/core/helpers/app_rejex.dart';
 import 'package:new_project/core/helpers/spacing.dart';
 import 'package:new_project/core/widgets/app_text_form_field.dart';
+import 'package:new_project/features/login/logic/cubit/login_cubit.dart';
+import 'package:new_project/features/login/ui/widgets/password_validations.dart';
 
 class EmailAndPassword extends StatefulWidget {
   const EmailAndPassword({super.key});
@@ -11,18 +15,49 @@ class EmailAndPassword extends StatefulWidget {
 
 class _EmailAndPasswordState extends State<EmailAndPassword> {
   bool isObSecured = true;
+  late TextEditingController passwordController;
+  bool hasLowerCase = false;
+  bool hasUpperCase = false;
+  bool hasSpecialChar = false;
+  bool hasNumber = false;
+  bool hasMminLength = false;
 
+  @override
+  void initState() {
+    super.initState();
+    passwordController = context.read<LoginCubit>().passwordEditingController;
+    setupPasswordControllerListner();
+  }
+
+  void setupPasswordControllerListner() {
+    passwordController.addListener(() {
+      setState(() {
+        hasLowerCase = AppRegex.hasLowerCase(passwordController.text);
+        hasMminLength = AppRegex.hasMinLength(passwordController.text);
+        hasUpperCase = AppRegex.hasUpperCase(passwordController.text);
+        hasSpecialChar = AppRegex.hasSpecialCharacter(passwordController.text);
+        hasNumber = AppRegex.hasNumber(passwordController.text);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: context.read<LoginCubit>().formKey,
       child: Column(
         children: [
           AppTextFormField(
             hintText: 'Email',
             textInputType: TextInputType.emailAddress,
             validator: (value) {
+              if (value == null ||
+                  value.isEmpty ||
+                  !AppRegex.isEmailValid(value)) {
+                return 'Please enter a valid email';
+              }
             },
+            controller: context.read<LoginCubit>().emailEditingController,
           ),
           verticalSpace(18),
           AppTextFormField(
@@ -43,12 +78,24 @@ class _EmailAndPasswordState extends State<EmailAndPassword> {
                 return 'please enter a valid password';
               }
             },
+            controller: context.read<LoginCubit>().passwordEditingController,
           ),
           verticalSpace(24),
-         
+          PasswordValidations(
+            hasLowerCase: hasLowerCase,
+            hasUpperCase: hasUpperCase,
+            hasSpecialChar: hasSpecialChar,
+            hasNumber: hasNumber,
+            hasMminLength: hasMminLength,
+          ),
         ],
       ),
     );
   }
 
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
 }
