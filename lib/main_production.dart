525cbc0f990705b24dde42d5fd6081e9cb8cd744
Marwa ELmasry <marwaelmasry592002@gmail.com>
2025-11-14 19:41:10 +0200

    - added Development and Production for Android flavors - added Android app name - added launch.json file

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:new_project/core/di/dependency_injection.dart';
 import 'package:new_project/core/routing/app_routes.dart';
 import 'doc_app.dart';
 void main() {
   setUpGetIt();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.amber,
+      statusBarColor: Colors.green
+    )
+  );
   runApp(DocApp(appRouter: AppRoutes(),));
 }
